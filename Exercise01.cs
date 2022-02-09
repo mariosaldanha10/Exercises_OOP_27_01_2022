2dc20b09e746295e9c5e447fe5479896f4546cfd
Mario Luis Saldanha Santos <mariosaldanha.ti@gmail.com>
2022-02-09 14:28:11 +0000

    Add project files.

---
+++ Exercise01.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Question01
+{
+	//constructor
+	public Question01()
+	{
+		
+		
+	}
+
+	public void simpleCalculator()
+    {
+        Console.WriteLine("Please enter first value: ");
+        int firstValue = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Please enter second value: ");
+        int secondValue = int.Parse(Console.ReadLine());
+
+        int resultSum = firstValue + secondValue;
+        int resultNeg = firstValue - secondValue;
+        int resultMul = firstValue * secondValue;
+        int resultDiv = firstValue / secondValue;
+        int resultRem = firstValue % secondValue;
+
+        Console.WriteLine("Summation: {0}\nSubtraction: {1}\nMultiplication : {2}\nDivision : {3}\nRemainder : {4}"
+            , resultSum, resultNeg, resultMul, resultDiv, resultRem);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Exercises_OOP_27_01_2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Question01 question01 = new();
+            question01.simpleCalculator();
+
+        }
+    }
+}
